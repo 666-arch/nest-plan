6995705f15a18f9f238bd8098b64b42ca4eaf1d0
MaxHuang <hc18174419502@163.com>
2024-01-27 15:34:16 +0800

    IOC/DI

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
+import { OtherService } from './other/other.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Inject(OtherService)
+  private otherService: OtherService;
+
+  
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return this.appService.getHello() + this.otherService.xxx();
   }
 }
 
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { AaaModule } from './aaa/aaa.module';
 import { XxxModule } from './xxx/xxx.module';
 import { PersonModule } from './person/person.module';
+import { OtherModule } from './other/other.module';
 
 @Module({
-  imports: [AaaModule, XxxModule, PersonModule],
+  imports: [AaaModule, XxxModule, PersonModule, OtherModule],
   controllers: [AppController],
   providers: [AppService],
 })
