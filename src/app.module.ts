6dcec9e859ff5994fb1927ae78ea1520d52b361e
666-arch <hc18174419502@163.com>
2024-01-28 17:03:35 +0800

    update 240128

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AaaModule } from './aaa/aaa.module';
 import { XxxModule } from './xxx/xxx.module';
 import { PersonModule } from './person/person.module';
 import { OtherModule } from './other/other.module';
 
 @Module({
-  imports: [AaaModule, XxxModule, PersonModule, OtherModule],
+  imports: [XxxModule, PersonModule, OtherModule],
   controllers: [AppController],
   providers: [AppService],
 })
